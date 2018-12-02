c7841c303ac33ebe1181a3c269b2e0e3a31046c7
Pickra <picker10024@yahoo.com>
2018-12-02 12:22:04 -0500

    Simplify Simple HTML doubleQuotesExample example + added a custom styles module for storyies reference

--- src/examples/Simple.stories.js
+++ src/examples/Simple.stories.js
@@ -1,29 +1,19 @@
 import { storiesOf } from '@storybook/html';
 import copyCodeBlock from '../copyCodeBlock';
+import custom from './custom';
 
 const singleQuotesExample = 
-`<div class='cool-container'>
-    <h1 class='cool-header'>HEY EVERYBODY</h1>
-    <p class='cool-description'>Get ready for...</p>
-    <ul class='cool-list'>
-        <li class='cool-item'>rainbows</li>
-        <li class='cool-item'>and</li>
-        <li class='cool-item'>unicorns</li>
-    </ul>
-</div>`;
+    `<div class='cool-container'>
+        <h1 class='cool-header'>HEY EVERYBODY</h1>
+        <p class='cool-description'>Get ready for...</p>
+        <ul class='cool-list'>
+            <li class='cool-item'>rainbows</li>
+            <li class='cool-item'>and</li>
+            <li class='cool-item'>unicorns</li>
+        </ul>
+    </div>`;
 
-const doubleQuotesExample = 
-`<div class="cool-container">
-    <h1 class="cool-header">HEY EVERYBODY</h1>
-    <p class="cool-description">Get ready for...</p>
-    <ul class="cool-list">
-        <li class="cool-item">rainbows</li>
-        <li class="cool-item">and</li>
-        <li class="cool-item">unicorns</li>
-    </ul>
-</div>`;
-
-const opts = { containerBackgroundColor: 'salmon' };
+const doubleQuotesExample = singleQuotesExample.replace(/'/g, '"');
 
 storiesOf('Simple HTML', module)
     .add('Single quotes', () => `
@@ -34,7 +24,7 @@ storiesOf('Simple HTML', module)
         ${doubleQuotesExample}
         ${copyCodeBlock(doubleQuotesExample)}
     `)
-    .add('Custom Colors', () => `
+    .add('Custom options', () => `
         ${doubleQuotesExample}
-        ${copyCodeBlock(doubleQuotesExample, opts)}
+        ${custom(doubleQuotesExample)}
     `);
---
+++ src/examples/custom.js
@@ -0,0 +1,5 @@
+import copyCodeBlock from '../copyCodeBlock';
+
+const opts = { containerBackgroundColor: 'salmon' };
+
+export default string => copyCodeBlock(string, opts);
