a6f9b73e90b96f92afa754f9a847b08d0f00d150
Kevin Groat <apps@kevingroat.io>
2018-12-08 14:24:39 -0500

    Improved stories

--- src/examples/html.stories.js
+++ src/examples/html.stories.js
@@ -1,15 +1,14 @@
+
 import { storiesOf } from '@storybook/html';
 import copyCodeBlock from '../copyCodeBlock';
 import customHtml from './customHtml';
-import customRust from './customRust';
 
 import hljs from 'highlight.js/lib/highlight';
 
+const hljsStyle = require('!url-loader!highlight.js/styles/a11y-light.css');
+
 // Register XML/HTML
 hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'));
-
-// Register Rust
-hljs.registerLanguage('rust', require('highlight.js/lib/languages/rust'));
 
 // Default hljs theme if no colors are supplied
 // import 'highlight.js/styles/a11y-dark.css';
@@ -27,15 +26,6 @@ const singleQuotesExample = `
 
 const doubleQuotesExample = singleQuotesExample.replace(/'/g, '"');
 
-const rustExample = `
-#![meta "meta-string"]
-fn main() {
-    // The statements here will be executed when the compiled binary is called
-
-    // Print text to the console
-    println!("Hello World!");
-}
-`;
 
 storiesOf('Simple HTML', module)
     .add('Single quotes', () => `
@@ -46,10 +36,12 @@ storiesOf('Simple HTML', module)
         ${doubleQuotesExample}
         ${copyCodeBlock(doubleQuotesExample, { lang: 'html' }) /* Will run through hljs, but without custom styling */}
     `)
-    .add('Custom options', () => `
+    .add('With syntax hilighting & built-in style', () => `
+        <link rel="stylesheet" href="${hljsStyle}">
+        ${doubleQuotesExample}
+        ${copyCodeBlock(doubleQuotesExample, { lang: 'html' }) /* Includes custom styling */}
+    `)
+    .add('With syntax hilighting & custom style', () => `
         ${doubleQuotesExample}
         ${customHtml(doubleQuotesExample) /* Includes custom styling */}
     `);
-
-storiesOf('Rust', module)
-    .add('Rust example', () => customRust(rustExample)) /* Includes custom styling */;
---
+++ src/examples/rust.stories.js
@@ -0,0 +1,44 @@
+
+import { storiesOf } from '@storybook/html';
+import copyCodeBlock from '../copyCodeBlock';
+
+import hljs from 'highlight.js/lib/highlight';
+
+// Register Rust
+hljs.registerLanguage('rust', require('highlight.js/lib/languages/rust'));
+
+const rustExample = `
+#![meta "meta-string"]
+fn main() {
+    // The statements here will be executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+}
+`.trim();
+
+
+const hljsOpts = {
+  lang: 'rust',
+  colors: {
+    foreground: '#222',
+    function: '#266',
+    title: '#a42',
+    comment: '#2a2',
+    built_in: '#a26',
+    string: '#288',
+    meta: '#555',
+    metaString: '#d2d' /* camelCase is converted to hyphen-case */
+  }
+};
+
+const noHljsOpts = {
+  colors: {
+    background: '#111',
+    foreground: '#efa',
+  }
+}
+
+storiesOf('Rust', module)
+  .add('Rust with syntax hilighting', () => copyCodeBlock(rustExample, hljsOpts))
+  .add('Rust without styntax hilighting', () => copyCodeBlock(rustExample, noHljsOpts));
