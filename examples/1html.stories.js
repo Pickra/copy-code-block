8989b678b5796a3cdafacea8fc511425ddabe1c0
Ari Picker <Pickra@users.noreply.github.com>
2018-12-14 14:58:57 -0500

    Feature/ccb dom element option (#8)
    Created 'return DOM element' feature

--- examples/1html.stories.js
+++ examples/1html.stories.js
@@ -2,29 +2,28 @@ import { storiesOf } from '@storybook/html';
 import copyCodeBlock from '../src/copyCodeBlock';
 import { customStyles, customHtml, opts } from './customHtml';
 import { usageExample, usageExampleJsHighlight } from './helpers';
+import * as htmlExample from './html-example.html';
 import hljs from 'highlight.js/lib/highlight';
 
 // Register languages for hljs
-hljs.registerLanguage('html', require('highlight.js/lib/languages/xml'));
+hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'));
 hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
 
 const a11yLightStyle = require('!url-loader!highlight.js/styles/a11y-light.css');
 const draculaStyle = require('!url-loader!highlight.js/styles/dracula.css');
 
-const htmlExample = `
-<div class='cool-container'>
-    <p class='cool-description'>Get ready for...</p>
-    <ul class='cool-list'>
-        <li class='cool-item'>rainbows</li>
-        <li class='cool-item'>and</li>
-        <li class='cool-item'>unicorns</li>
-    </ul>
-</div>`;
-
 storiesOf('HTML', module)
-    .add('Default', () => `
+    .add('Code File', () => `
         <link rel="stylesheet" href="${draculaStyle}">
-        <h1>Default</h1>
+        <h1>Code File</h1>
+        <h2>Example Code</h2>
+        ${copyCodeBlock(htmlExample) /* Will not run through hljs */}
+        <h2>Usage</h2>
+        ${copyCodeBlock(usageExample(null, 'html'), usageExampleJsHighlight)}
+    `)
+    .add('Code String', () => `
+        <link rel="stylesheet" href="${draculaStyle}">
+        <h1>Code String</h1>
         <h2>Example Code</h2>
         ${copyCodeBlock(htmlExample) /* Will not run through hljs */}
         <h2>Usage</h2>
@@ -61,4 +60,29 @@ storiesOf('HTML', module)
         ${customHtml(htmlExample) /* Run through hljs, w/custom styles */}
         <h2>Usage</h2>
         ${copyCodeBlock(usageExample(opts), usageExampleJsHighlight)}
-    `);
+    `)
+    .add('Return DOM element', () => {
+        const options = {shouldReturnDomEl: true};
+        const container = document.createElement('div');
+
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = draculaStyle;
+
+        const titleHeader = document.createElement('h1');
+        titleHeader.innerHTML = 'Return DOM element';
+
+        const exampleHeader = document.createElement('h2');
+        exampleHeader.innerHTML = 'Example Code';
+
+        const domEl = copyCodeBlock(htmlExample, options);
+
+        const usageHeader = document.createElement('h2');
+        usageHeader.innerHTML = 'Usage Code';
+
+        const jsUsageExample = document.createElement('div');
+        jsUsageExample.innerHTML = copyCodeBlock(usageExample(options), usageExampleJsHighlight);
+
+        container.append(link, titleHeader, exampleHeader, domEl, usageHeader, jsUsageExample);
+        return container;
+    });
